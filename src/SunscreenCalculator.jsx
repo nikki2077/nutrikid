46570e55fae08a20fabc9f03300397e025173b9b
nikki <pwuu0021@student.monash.edu>
2024-03-15 12:21:31 +1100

    testing

---
+++ src/SunscreenCalculator.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './SunscreenCalculator.css'
+
+export default function SunscreenCalculator(){
+    return(
+        <div className='comming-soon'>
+            <img src="./Sunscreen_Calculator.png" alt="" />
+    </div>
+    )
+}
