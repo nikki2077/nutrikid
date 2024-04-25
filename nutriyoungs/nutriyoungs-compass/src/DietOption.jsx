968511e7ec9037d0ace40978d20ca49096ab31c6
nikki <pwuu0021@student.monash.edu>
2024-04-25 16:10:59 +1000

    genrate button

--- nutriyoungs/nutriyoungs-compass/src/DietOption.jsx
+++ nutriyoungs/nutriyoungs-compass/src/DietOption.jsx
@@ -3,7 +3,7 @@ import './DietOption.css';
 
 
 const DietOption = ({ onToggleVisibility, onSubmitPreferences }) => {
-    const [calorieNeeds, setCalorieNeeds] = useState(2500);
+    const [calorieNeeds, setCalorieNeeds] = useState(2000);
     const [selectedCuisine, setSelectedCuisine] = useState('');
     const [selectedDietHabit, setSelectedDietHabit] = useState('');
 
@@ -52,7 +52,7 @@ const DietOption = ({ onToggleVisibility, onSubmitPreferences }) => {
                         step="100"
                     />
                     <div className="calorie-values">
-                        <span>0</span>
+                        <span>1000</span>
                         <span>{calorieNeeds}</span>
                         <span>3000</span>
                     </div>
